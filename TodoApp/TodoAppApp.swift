fd6b1a3e4cd9a45adf3a83333859e9b135252284
Mohammad Azam <azamsharp@gmail.com>
2021-03-29 10:04:39 -0500

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Mohammad Azam on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
